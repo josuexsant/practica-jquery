043c88c2c2c010bccdfc878e76923484e612d941
Josue Santamaria <josuexsanta@gmail.com>
2024-04-29 13:34:20 -0600

    El servidor responde con JSON

--- php/gananciaAleatoria.php
+++ php/gananciaAleatoria.php
@@ -5,6 +5,12 @@ $xmlData = file_get_contents( 'php://input' );
 $xml = simplexml_load_string( $xmlData );
 $nombre = isset( $xml->nombre ) ? ( string )$xml->nombre : 'Anónimo';
 $gananciaMaxima = rand( 0, 100 );
-echo 'Felicidades Sr. ' . $nombre . ', has ganado $' . $gananciaMaxima . ' pesos';
+
+$response = [
+    'mensaje' => 'Felicidades Sr. ' . $nombre . ', has ganado $' . $gananciaMaxima . ' pesos'
+];
+
+header( 'Content-Type: application/json' );
+echo json_encode( $response );
 
 ?>
